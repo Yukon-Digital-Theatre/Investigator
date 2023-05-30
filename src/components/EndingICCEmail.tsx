f2c387b7b5cef66a5aefd049c028c97fc50d9400
YukonDigitalTheatre <134099445+YukonDigitalTheatre@users.noreply.github.com>
2023-05-30 22:56:36 +0100

    Update EndingICCEmail.tsx
    paragraphs & formatting

--- src/components/EndingICCEmail.tsx
+++ src/components/EndingICCEmail.tsx
@@ -4,14 +4,15 @@ const EndingICCEmail = () => {
   return (
   
     
-    <div className='introText letter'>Email from 	Email from the International Criminal Court
-		Subject: Exit Interview November 15
-		Hello,
-Just wanted to quickly let you know prior to your exit interview we are particularly interested in any notes or recollections you have from your interrogation of Weaver #11. We understand this suspect ran away and was never apprehended. While we have released a number of weavers due to insufficient evidence there are a handful we are detaining as there was a consultation process in which some weavers were told about the origins of the yarn. We have identified a number of weavers who participated in this process, but several remain unidentified. We have descriptions of some of these and believe Weaver #11 may be a match.
+    <div className='introText letter'>
+		  <p><b>Email from the International Criminal Court</b></p>
+		  <p><b>Subject: Exit Interview November 15</b></p>
+		  <p>Hello,</p>
+<p>Just wanted to quickly let you know prior to your exit interview we are particularly interested in any notes or recollections you have from your interrogation of Weaver #11. We understand this suspect ran away and was never apprehended. While we have released a number of weavers due to insufficient evidence there are a handful we are detaining as there was a consultation process in which some weavers were told about the origins of the yarn. We have identified a number of weavers who participated in this process, but several remain unidentified. We have descriptions of some of these and believe Weaver #11 may be a match.</p>
 
-Thank you in advance,
+		  <p>Thank you in advance,</p>
 
-ICC Administration
+		  <p>ICC Administration</p>
 
 </div>
 
