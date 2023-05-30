e155b688201c32eb2a971189c98c3d058c41393c
YukonDigitalTheatre <134099445+YukonDigitalTheatre@users.noreply.github.com>
2023-05-30 22:13:16 +0100

    Update IntroQFive.tsx
    text edit

--- src/pages/IntroQFive.tsx
+++ src/pages/IntroQFive.tsx
@@ -24,10 +24,10 @@ function helperTwo(){
     <div>
     <p className='outlineText introBigText'>This is a world out of time and place. It is similar to our own.
 	However, this country, in this world, is recovering from a war stretching over several years.
-	The war ended only six weeks ago.
+	    The war ended only six weeks ago.</p>
 
 
-    Would you like to read more about the history of this world before continuing?</p>
+    <p className='outlineText introBigText'>Would you like to learn more about the history of this world before continuing?</p>
     <p className='introText outlineText Button' onClick={()=>helper()}>Yes</p>
     <p className='introText outlineText Button' onClick={()=>helperTwo()}>No</p>
     </div>
