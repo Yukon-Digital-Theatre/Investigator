34c95b5d7d8bda52a044c5c29c5dc3b9733fb697
HRSPA <hrsparro@gmail.com>
2023-06-08 17:36:48 -0600

    Loading Issue Fix

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,13 +1,11 @@
 import { useDispatch } from 'react-redux';
 import { updatePage } from '../reducers/currentPage/currentPageSlice';
-import { memo, useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { updateHalo } from '../reducers/haloMode/haloModeSlice';
 import { updateInvAudioDelivery } from '../reducers/invAudioDeliveryMode/invAudioDeliveryModeSlice';
 import { updateNarratorAudioDelivery } from '../reducers/narratorAudioDeliveryMode/narratorAudioDeliveryModeSlice';
 import { updateTextDelivery } from '../reducers/textDeliveryMode/textDeliveryModeSlice';
-import { backgroundAudio } from '../data/backgroundAudioData';
 import { narratorAudio } from '../data/narratorAudioData';
-import { Rings } from 'react-loader-spinner'
 
 const Intro = () => {
 
@@ -16,7 +14,6 @@ const dispatch= useDispatch();
 const [style, setStyle] = useState(false)
 const [loaded, setLoaded] = useState(false)
 let loadAmount=0;
-const [ellipses, setEllipses] = useState(true)
 const [loadPercent, setLoadPercent] = useState(0)
 dispatch(updateHalo(0));
 dispatch(updateInvAudioDelivery(true))
@@ -34,36 +31,6 @@ dispatch(updateTextDelivery(true));
 
 
 
-  
- intid=setInterval(() => {
-    
-let percent =0;
-
-    let check=true;
-    
-   
- 
-  narratorAudio.map((item)=>{item.audio.state()==="loading"?(check=false):(percent+=1)});
-  
-if(loadPercent<percent){
-loadAmount=(percent);
-setLoadPercent(loadAmount/29);
-}
-  if(check){
-
-    setLoaded(true);
-    clearInterval(intid);
-  
-  }
-  
-    
-console.log("Loading...")
-  
-  }, 500);
-
-
-
-
 
 function helper(){
   setStyle(true);
@@ -72,6 +39,50 @@ function helper(){
 
   }, 2000);
 }
+
+
+
+  
+
+
+
+
+
+ intid=setInterval(() => {
+    
+  let percent =0;
+  
+      let check=true;
+      
+     
+   
+    narratorAudio[0].audio.state()==="loading"?(check=false):(percent+=1);
+    narratorAudio[1].audio.state()==="loading"?(check=false):(percent+=1);
+    narratorAudio[2].audio.state()==="loading"?(check=false):(percent+=1);
+    narratorAudio[3].audio.state()==="loading"?(check=false):(percent+=1);
+    narratorAudio[4].audio.state()==="loading"?(check=false):(percent+=1);
+    narratorAudio[5].audio.state()==="loading"?(check=false):(percent+=1);
+    narratorAudio[6].audio.state()==="loading"?(check=false):(percent+=1);
+    narratorAudio[7].audio.state()==="loading"?(check=false):(percent+=1);
+    narratorAudio[8].audio.state()==="loading"?(check=false):(percent+=1);
+    
+  
+    
+  if(loadPercent<percent){
+  loadAmount=(percent);
+  setLoadPercent(loadAmount/9);
+  }
+    if(check){
+  
+      setLoaded(true);
+      clearInterval(intid);
+    
+    }
+    
+      
+  console.log("Loading...")
+    
+    }, 100);
 
 
 
